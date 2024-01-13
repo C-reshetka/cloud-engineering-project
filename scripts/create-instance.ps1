c36f92a974c87cfdaf3a97d0f0a5637f6704102c
i.krasheninnikov <i.krasheninnikov@skbkontur.ru>
2024-01-14 01:29:10 +0500

    add create-instance script

---
+++ scripts/create-instance.ps1
@@ -0,0 +1,14 @@
+param($PubKeyPath)
+
+$create_vm_output = yc compute instance create --public-ip `
+    --ssh-key $PubKeyPath `
+    --create-boot-disk image-id=fd8msa98ll42djaaajg9 `
+    --core-fraction 20;
+
+$instance_id = ([regex]'id: (.*?) ').Matches($create_vm_output).Groups[1];
+$instance_internal_ip = ([regex]' address: (.*?) ').Matches($create_vm_output)[0].Groups[1];
+$instance_subnet_id = ([regex]' subnet_id: (.*?) ').Matches($create_vm_output)[0].Groups[1];
+
+yc load-balancer target-group update `
+    --id enpbetinst7moo3bqvkk `
+    --target address=$instance_internal_ip,subnet-id=$instance_subnet_id
